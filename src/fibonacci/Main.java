71d7fcfc0aee5df6f6e72b26e63500189436df48
Garrett Rowell <garrettrowell@gmail.com>
2016-01-31 21:13:43 -0500

    allow cli arguments

--- src/fibonacci/Main.java
+++ src/fibonacci/Main.java
@@ -2,13 +2,18 @@ package fibonacci;
 
 public class Main {
 	public static void main(String[] args) {
-		java.util.Scanner input = new java.util.Scanner(System.in);
-		System.out.println("Enter the nth term to calculate the Fibonacci Sequence to:");
-		int n = input.nextInt();
-		for(int i =0; i<=n; i++) {
-			System.out.print(" " + BinetsFormula(i));
+		int n;
+		if (args.length >0) {
+			n = Integer.parseInt(args[0]);
+		} else {
+			java.util.Scanner input = new java.util.Scanner(System.in);
+			System.out.println("Enter the nth term to calculate the Fibonacci Sequence to:");
+			n = input.nextInt();
+			input.close();
 		}
-		input.close();
+		for(int i =0; i<=n; i++) {
+			System.out.println("Fibbo(" + i + ") = " + BinetsFormula(i));
+		}
 	}
 	
 	public static int BinetsFormula(int n) {
